90be98db197e1da3d404ccb358b6ddc80a90df83
David Airisniemi <David.Airisniemi@hotmail.com>
2022-05-19 09:44:32 +0200

    Updated properties

--- TimeReportingAPI/Data/TimeAndDate.cs
+++ TimeReportingAPI/Data/TimeAndDate.cs
@@ -7,7 +7,7 @@
         public int AmountOfHours { get; set; }
         public int AmountOfMinutes { get; set; }
         public string Description { get; set; }
-        public int CustomerId { get; set; }
-        public int ProjectId { get; set; }
+        public Customer Customer { get; set; }
+        public Project Project { get; set; }
     }
 }
